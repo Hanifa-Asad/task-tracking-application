85793deeec4f0737f00e0d736ad84363cde72b50
Hanifa-Asad <hanifaasasd23@gmail.com>
2025-04-28 02:21:00 +0500

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,11 @@ app.use(cookieParser(secret));
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Hardcoded for now
+  	origin: ['http://localhost:5174',
+      'http://localhost:5173',
+     'https://task-tracking-application-bcgv.vercel.app',
+     
+     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
