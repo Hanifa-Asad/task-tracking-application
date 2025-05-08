ac773a9b7f21bd29932fd4c2139ad1385e275cdf
Hanifa-Asad <158709126+Hanifa-Asad@users.noreply.github.com>
2025-05-09 00:03:45 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,45 +8,36 @@ import taskRoutes from "./routes/taskRoutes.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
 const secret = process.env.COOKIE_SECRET;
-const frontendBaseURL = process.env.FRONTEND_BASE_URL;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(secret));
 
-
-
-
 app.use(
   cors({
-  	origin: ['http://localhost:5174',
-      'http://localhost:5173',
-     'https://task-tracking-application-bcgv.vercel.app',
-     
-     ],
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://task-tracking-application-bcgv.vercel.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello, Welocome To Vooshfoods" });
+  res.json({ message: "Hello, Welcome to Vooshfoods" });
 });
 
-app.listen(PORT, () => {
-  connectDB()
-    .then(() => console.log(`Server started on PORT: ${PORT}`))
-    .catch((error) => {
-      console.error('Failed to connect to DB', error);
-      process.exit(1); // Force exit the app
-    });
-});
 
+const handler = async (req, res) => {
+  await connectDB(); // Ensure DB is connected for each invocation
+  return app(req, res);
+};
+
+export default handler;
