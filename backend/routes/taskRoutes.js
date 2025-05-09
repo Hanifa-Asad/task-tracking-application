868dfc7a31d910a960b6ec30dc0d3725db46c1ad
Hanifa-Asad <158709126+Hanifa-Asad@users.noreply.github.com>
2025-05-10 02:07:13 +0500

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,17 +1,25 @@
+import express from "express";
 import {
   getTasks,
-  getTaskById, 
+  getTaskById,
   createTask,
   updateTask,
   deleteTask,
   reorderTasks,
 } from "../controllers/taskController.js";
+// import authMiddleware from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
+
+// Optional: protect all task routes
 // router.use(authMiddleware);
 
+// Routes
 router.get("/", getTasks);
-router.get("/:id", getTaskById); 
+router.get("/:id", getTaskById); // optional
 router.post("/", createTask);
 router.put("/reorder", reorderTasks);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
+
+export default router;
